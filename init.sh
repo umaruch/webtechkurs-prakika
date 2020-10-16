a889a13a4b29f4cfe0ebdaf9fbbeead25909049c
umaruch <ezzik1998@gmail.com>
2020-10-16 18:30:56 +0500

    add wsgi app with g unicorn

--- init.sh
+++ init.sh
@@ -1,3 +1,4 @@
 sudo ln -sf /home/umaruch/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
 sudo rm -rf /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
+gunicorn -b 0.0.0.0:8080  myapp:app
