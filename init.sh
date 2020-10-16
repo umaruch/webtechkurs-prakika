7be02110bd60454a36a3689d5b9452d3bbe01f01
umaruch <ezzik1998@gmail.com>
2020-10-17 00:01:28 +0500

    add django app

--- init.sh
+++ init.sh
@@ -1,4 +1,5 @@
 sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
 sudo rm -rf /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
-gunicorn -b '0.0.0.0:8080' hello:app
+gunicorn -b '0.0.0.0:8080' -D hello:app
+gunicorn -c /home/box/web/etc/django_config.py -D ask.wsgi:application
